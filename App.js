c9bc7e3c8cbaa0eea460b4e68409d5dac9c470ea
Ricardo Aldahir Garcia Romero <22470066@campeche.tecnm.mx>
2025-11-16 20:36:44 -0600

    versión final del ShopList

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
+import ItemInput from './Components/ItemInput';
+import ShopListItem from './Components/ShopListItem';
+import { useState } from 'react';
 
 export default function App() {
+  const [items, setItems] = useState([
+
+  {
+    id: Math.random().toString(),
+    title: 'Pan',
+    },
+  {
+    id: Math.random().toString(),
+    title: 'Leche',
+  },
+  {
+    id:Math.random().toString(),
+    title: 'Jamón',
+  },
+
+
+
+  ]);
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+      <ItemInput 
+      onPress={(enteredText)=>{
+        setItems (currentItems => [
+          ...currentItems,
+            {
+              id: Math.random().toString(),
+              title: enteredText,
+           },
+            ]);
+          }}
+          />
+      </View>
+
+      <View style={styles.listContainer}>
+        <FlatList
+          data={items}
+          renderItem={
+            ({item})=><ShopListItem title={item.title}/>
+          }
+          keyExtractor={item => item.id
+          }
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 32,
+    gap: 8,
   },
+  listContainer: {
+    flex: 1,
+    backgroundColor: '#ddd',
+  }
 });
